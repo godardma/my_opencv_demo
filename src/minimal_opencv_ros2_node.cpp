b4d3b68eb17af20d7b4286f9561720334b691b79
godardma <mael.godard@ensta-bretagne.org>
2023-10-18 10:14:02 +0200

    ellipsoid filter

--- src/minimal_opencv_ros2_node.cpp
+++ src/minimal_opencv_ros2_node.cpp
@@ -6,16 +6,80 @@
 #include <cv_bridge/cv_bridge.h> // cv_bridge converts between ROS 2 image messages and OpenCV image representations.
 #include <image_transport/image_transport.hpp> // Using image_transport allows us to publish and subscribe to compressed image streams in ROS2
 #include <opencv2/opencv.hpp> // We include everything about OpenCV as we don't care much about compilation time at the moment.
- 
+
 using namespace std::chrono_literals;
 using namespace std;
 using namespace cv;
 RNG rng(12345);
-// VideoWriter video("video_10_11_1.avi",VideoWriter::fourcc('M','J','P','G'),10, Size(680,480));
+// VideoWriter video("test_tps_reel.avi",VideoWriter::fourcc('M','J','P','G'),10, Size(680,480));
 int i=0;
+
+// the median 
+double findMedian(vector<int> a, 
+                  int n) 
+{ 
+  
+    // If size of the arr[] is even 
+    if (n % 2 == 0) { 
+  
+        // Applying nth_element 
+        // on n/2th index 
+        nth_element(a.begin(), 
+                    a.begin() + n / 2, 
+                    a.end()); 
+  
+        // Applying nth_element 
+        // on (n-1)/2 th index 
+        nth_element(a.begin(), 
+                    a.begin() + (n - 1) / 2, 
+                    a.end()); 
+  
+        // Find the average of value at 
+        // index N/2 and (N-1)/2 
+        return (double)(a[(n - 1) / 2] 
+                        + a[n / 2]) 
+               / 2.0; 
+    } 
+  
+    // If size of the arr[] is odd 
+    else { 
+  
+        // Applying nth_element 
+        // on n/2 
+        nth_element(a.begin(), 
+                    a.begin() + n / 2, 
+                    a.end()); 
+  
+        // Value at index (N/2)th 
+        // is the median 
+        return (double)a[n / 2]; 
+    } 
+} 
+
+Mat DoubleMatFromVec3b(Vec3b in)
+{
+    cv::Mat_ mat(3,1, CV_64FC1);
+    mat.at <int>(0,0) = int(in [0]);
+    mat.at <int>(1,0) = int(in [1]);
+    mat.at <int>(2,0) = int(in [2]);
+
+    return mat;
+};
+
 class MinimalImageSubscriber : public rclcpp::Node {
 public:
   MinimalImageSubscriber() : Node("opencv_image_publisher"), count_(0) {
+    namedWindow("source",WINDOW_NORMAL);
+    resizeWindow("source", 1000, 700);
+    Gamma = (Mat_<double>({4.7905734e-04, 1.9332248e-04 ,3.6835161e-04, 1.9332266e-04, 1.8628560e-04, 3.1789717e-05, 3.6835123e-04 ,3.1789317e-05, 7.3987240e-04})).reshape(3);
+    cout << "Gamma = " << endl << " " << Gamma << endl << endl;
+    centroid = Mat_<double>({135.98308 , 33.28846, 218.4584});
+    cout << "centroid = " << endl << " " << centroid << endl << endl;
+    // Gamma = (Mat_<double>({2.9627589e-04 , 1.2542386e-04 , 4.0035873e-05,1.2542389e-04  ,1.6761490e-04 ,-4.0089159e-05, 4.0035862e-05 ,-4.0089169e-05 , 1.0725303e-04})).reshape(3);
+    // cout << "Gamma = " << endl << " " << Gamma << endl << endl;
+    // centroid = Mat_<double>({124.14576 ,  28.179985 ,200.27927});
+    // cout << "centroid = " << endl << " " << centroid << endl << endl;
+
     subscriber_ =
         this->create_subscription<sensor_msgs::msg::CompressedImage>("comp_image", 10, std::bind(&MinimalImageSubscriber::frame_callback, this, std::placeholders::_1));
   }
@@ -27,6 +91,8 @@ private:
     auto frame=cv_bridge::toCvCopy(*msg.get(), "bgr8");
     // Mat_ ??
     auto src=frame->image;
+    
+
 
     // std::cout << src.at<double>(0,0)<<std::endl;
     double angle = 90;
@@ -38,31 +104,95 @@ private:
 
     cv::Mat dst;
     cv::warpAffine(src, dst, rot, bbox.size());
+
     cv::Mat dst_blur;
     blur(dst,dst_blur,Size(10,10));
     cv::Mat fullImageHSV;
     cvtColor(dst_blur, fullImageHSV, CV_BGR2HSV);
+    cv::Mat mask = cv::Mat::zeros(dst_blur.size(), CV_64FC1);
+    vector<int> vec_i,vec_j;
+    // cout<<mask.at<double>(0,0)<<endl;
+    for (int i=0;i<fullImageHSV.rows;i=i+3){
+      for (int j=0;j<fullImageHSV.rows;j=j+3){
+        auto hsv_elem = DoubleMatFromVec3b(fullImageHSV.at<Vec3b>(i,j));
+        // cv::Mat_ xyz(3,1, CV_64FC1);
+        double theta = (double) 2.0*hsv_elem.at <int>(0,0)*M_PI/180.0;
+        double r = (double) hsv_elem.at<int>(0,1);
+        double z = (double) hsv_elem.at<int>(0,2);
+        Mat_<double> xyz = Mat_<double>({r*cos(theta)  ,r*sin(theta) , z});
+
+        auto inter=xyz-centroid;
+        auto inter2 = inter.t()*Gamma*inter;
+        double boo=norm(inter2)<1;
+        if (boo){
+          vec_i.insert(vec_i.end(),i);
+          vec_j.insert(vec_j.end(),j);
+        }
+        mask.at<double>(i,j)=boo;
+        
+      }
+    }
+    
     
 
 
 
     //Traitement de l'image HSV ici
 
-    cv::Mat copy = dst_blur.clone();
-    cv::Mat mask = cv::Mat::zeros(dst_blur.size(), dst_blur.type());
+    // cv::Mat copy = dst_blur.clone();
+    // cv::Mat mask = cv::Mat::zeros(dst_blur.size(), dst_blur.type());
 
-    //Define your destination image
-    cv::Mat dstImage = cv::Mat::zeros(dst_blur.size(), dst_blur.type()); 
-    cv::Mat dstImage_bis = cv::Mat::zeros(dst_blur.size(), dst_blur.type());    
+    // //Define your destination image
+    // cv::Mat dstImage = cv::Mat::zeros(dst_blur.size(), dst_blur.type()); 
+    // cv::Mat dstImage_bis = cv::Mat::zeros(dst_blur.size(), dst_blur.type());    
 
 
-    inRange(fullImageHSV, Scalar(150, 120, 40), Scalar(360, 360, 360), dstImage);
-    inRange(fullImageHSV, Scalar(0, 120, 40), Scalar(30, 360, 360), dstImage_bis);
-    dstImage=dstImage+dstImage_bis;
+    // inRange(fullImageHSV, Scalar(150, 120, 40), Scalar(360, 360, 360), dstImage);
+    // inRange(fullImageHSV, Scalar(0, 120, 40), Scalar(30, 360, 360), dstImage_bis);
+    // dstImage=dstImage+dstImage_bis;
 
-    int morph_size = 3; 
+    // int morph_size = 3; 
   
-    // Create structuring element 
+    // // Create structuring element 
+    // Mat element = getStructuringElement( 
+    //     MORPH_ELLIPSE, 
+    //     Size(2 * morph_size + 1, 
+    //          2 * morph_size + 1), 
+    //     Point(morph_size, morph_size)); 
+  
+    // // Closing 
+    // cv::Mat closedImage = cv::Mat::zeros(dst.size(), dst.type()); 
+    // morphologyEx(dstImage, closedImage, 
+    //              MORPH_OPEN, element, 
+    //              Point(-1, -1), 2); 
+
+    // Mat element_bis = getStructuringElement( 
+    //     MORPH_ELLIPSE, 
+    //     Size(2 * 4 + 1, 
+    //          2 * 4 + 1), 
+    //     Point(4, 4)); 
+    // cv::Mat openedImage = cv::Mat::zeros(dst.size(), dst.type()); 
+    // morphologyEx(dstImage, openedImage, 
+    //              MORPH_CLOSE, element_bis, 
+    //              Point(-1, -1), 2); 
+    // vector<vector<Point> > contours;
+    // vector<Vec4i> hierarchy;
+    // findContours( openedImage, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE );
+    // // Mat drawing = Mat::zeros( openedImage.size(), CV_8UC3 );
+    // vector<Point2f>centers( contours.size() );
+    // vector<float>radius( contours.size() );
+    // for( size_t i = 0; i< contours.size(); i++ )
+    // {
+    //   Scalar color = Scalar( 0, 255, 0 );
+    //   Scalar color_red = Scalar( 255, 0, 255 );
+    //   minEnclosingCircle( contours[i], centers[i], radius[i] );
+    //   // circle( dst, centers[i], (int)radius[i], color_red, 2 );
+      
+    //   // drawContours( dst, contours, (int)i, color, 2, LINE_8, hierarchy, 0 );
+    // }
+    // imshow( "Contours", drawing );
+
+    int morph_size = 2;
     Mat element = getStructuringElement( 
         MORPH_ELLIPSE, 
         Size(2 * morph_size + 1, 
@@ -71,48 +201,36 @@ private:
   
     // Closing 
     cv::Mat closedImage = cv::Mat::zeros(dst.size(), dst.type()); 
-    morphologyEx(dstImage, closedImage, 
-                 MORPH_OPEN, element, 
-                 Point(-1, -1), 2); 
-
-    Mat element_bis = getStructuringElement( 
-        MORPH_ELLIPSE, 
-        Size(2 * 4 + 1, 
-             2 * 4 + 1), 
-        Point(4, 4)); 
-    cv::Mat openedImage = cv::Mat::zeros(dst.size(), dst.type()); 
-    morphologyEx(dstImage, openedImage, 
-                 MORPH_CLOSE, element_bis, 
-                 Point(-1, -1), 2); 
-    vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
-    findContours( openedImage, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE );
-    // Mat drawing = Mat::zeros( openedImage.size(), CV_8UC3 );
-    vector<Point2f>centers( contours.size() );
-    vector<float>radius( contours.size() );
-    for( size_t i = 0; i< contours.size(); i++ )
-    {
-      Scalar color = Scalar( 0, 255, 0 );
-      Scalar color_red = Scalar( 255, 0, 255 );
-      minEnclosingCircle( contours[i], centers[i], radius[i] );
-      circle( dst, centers[i], (int)radius[i], color_red, 2 );
+    morphologyEx(mask, closedImage, 
+                 MORPH_CLOSE, element, 
+                 Point(-1, -1), 2);
+    Scalar color_pink = Scalar( 0,255,0 );
+    if (vec_i.size()!=0){
+      auto mid_i=findMedian(vec_i,vec_i.size());
+      auto mid_j=findMedian(vec_j,vec_j.size());
+      Point pt = Point(mid_j, mid_i);
       
-      // drawContours( dst, contours, (int)i, color, 2, LINE_8, hierarchy, 0 );
+      circle( dst, pt, 4, color_pink, 2 );
+      // cout<<"MID"<<mid_i<<" "<<mid_j<<endl;
     }
-    // imshow( "Contours", drawing );
+    
+
+    
     cv::imshow("source", dst );
-    Mat destin = Mat::zeros(dst.size(), dst.type());    
-    dst.copyTo(destin, openedImage);
-    // if (i<104){video.write(destin);i++;cout<<i<<endl;}
+    cv::imshow("mask", closedImage );
+    // Mat destin = Mat::zeros(dst.size(), dst.type());    
+    // dst.copyTo(destin, openedImage);
+    // if (i<104){video.write(dst);i++;cout<<i<<endl;}
     // else if (i==104){video.release();cout<<"FINIIIIIIIIIIIIIIII"<<endl;}
     
-    cv::imshow("closed", openedImage );
+    // cv::imshow("closed", openedImage );
     char c=(char)cv::waitKey(25);
 
   }
   sensor_msgs::msg::CompressedImage::SharedPtr msg_comp;
   rclcpp::Subscription<sensor_msgs::msg::CompressedImage>::SharedPtr subscriber_;
   size_t count_;
+  Mat_<double> Gamma;Mat_<double> centroid;
 };
  
 int main(int argc, char *argv[]) {
